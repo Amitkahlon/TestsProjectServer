a681719fe6102f190a5f3bcd03a5a62736ecf606
Roee Cohen <roeec84@gmail.com>
2021-02-06 12:04:32 +0200

    Updated fields.js

--- routes/fields.js
+++ routes/fields.js
@@ -33,4 +33,33 @@ router.post('/', auth, async (req, res) => {
     }
 })
 
+router.put('/:id', auth, async (req, res) => {
+    const { id } = req.params
+    const {field} = req.body
+    const {error} = validateField(field)
+    if(error) return res.send({message: error.details[0].message}).status(403)
+    try {
+        const updatedField = await Field.findByIdAndUpdate(id, {
+            $set: {
+                title: field.title
+            }
+        }, { new: true, useFindAndModify: false })
+        if (!updatedField) return res.status(404).send({ message: "Field not found." })
+        res.status(200).send({ field: updatedField });
+    } catch (err) {
+        res.send(err).status(403)
+    }
+})
+
+router.delete('/:id', auth, async (req, res) => {
+    const { id } = req.params;
+    try {
+        const deletedField = await Field.findByIdAndRemove(id, { useFindAndModify: false })
+        if (!deletedField) return res.status(404).send({ message: "Field not found" });
+        res.status(200).send({ field: deletedField })
+    } catch (error) {
+        res.status(404).send({ message: `Field with id ${id} not removed`, error })
+    }
+})
+
 module.exports = router
